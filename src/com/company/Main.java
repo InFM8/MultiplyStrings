a9123eeda19e1656fe7225dd94bae5f1ff685f3d
InFM8 <andriusm4@gmail.com>
2021-12-21 12:07:09 +0200

    Paredaguotas projektas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,68 +7,60 @@ public class Main {
         String num1 = "123";
         String num2 = "456";
 
-        System.out.println(num1 + " * " + num2 + " = " +multiply(num1, num2));
-
+        System.out.println(num1 + " * " + num2 + " = " + multiply(num1, num2));
     }
 
     /**
-     * Funkcija skaiciuojanti pirmo ir antro skaiciaus daugyba
+     * Funkcija skaiciuojanti pirmo ir antro skaiciaus sandauga
+     *
      * @param num1 pirmas skaicius
      * @param num2 antras skaicius
      * @return grazinamas atsakymas
      */
+
     public static String multiply(String num1, String num2) {
 
-        if(num1.equals("0") || num2.equals("0")) {
+        if (num1.equals("0") || num2.equals("0")) {
             return "0";
         }
+
         StringBuilder sk1 = new StringBuilder(num1);
         StringBuilder sk2 = new StringBuilder(num2);
 
-        //Abu skaicius apverciame
-        sk1.reverse();
-        sk2.reverse();
+        sk1.reverse(); // 321
+        sk2.reverse(); // 654
 
-        //Issaugome sandaugos rezultata kiekvieno skaitmens antro ir pirmo skaiciaus.
-
-        int rez = sk1.length() + sk2.length();
+        int rez = sk1.length() + sk2.length(); // rez = 6;
 
         StringBuilder ats = new StringBuilder();
-        for(int i = 0; i < rez; ++i) {
-            ats.append(0);
+        for (int i = 0; i < rez; ++i) {
+            ats.append(0);                    // ats = 000000;
         }
-        for(int i = 0; i < sk2.length(); i++) {
-            int skaitmuo2 = sk2.charAt(i) - '0';
+        for (int i = 0; i < sk2.length(); i++) {
+            int skaitmuo2 = sk2.charAt(i) - '0';     //skaitmuo2 = 6, 5, 4;
 
-//kiekviena skaitmeni antro skaiciaus dauginame visus skaitmenis pirmo skaiciaus (dauginimas stulpeliu)
 
             for (int j = 0; j < sk1.length(); j++) {
-                int skaitmuo1 = sk1.charAt(j) - '0';
+                int skaitmuo1 = sk1.charAt(j) - '0'; //skaitmuo1 = 3, 2, 1
 
-                // Nuliniai skaiciai priklauso nuo vietos antro skaitmens, antro skaiciaus ir
-                // pirmo skaitmens, pirmo skaiciaus.
+                int dabartinePozicija = j + i; //0,1,2,1,2,3,2,3,4
 
-                int dabartinePozicija = j + i;
+                int saugoti = ats.charAt(dabartinePozicija) - '0'; //000 (9x)
+                int daugyba = skaitmuo1 * skaitmuo2 + saugoti; //18, 12, 6, 15, 10, 5, 12, 8, 4
 
-                // Skaitmens "dabartinePozicija" pozicija siuo metu Stringo "ats"
-                // inicijuojame dabartinePozicija - 'saugoti' ir sumuojame su esamu rezultatu
+                //Nustatoma kiekvieno skaitmens vieta rezultate
+                ats.setCharAt(dabartinePozicija, (char) (daugyba % 10 + '0'));
+                                                                         //800000, 820000, 826000, 876000,
+                                                                         //876000, 876500, 878500, 878300, 878340
+                // Issaugome daugybos rezultata tarp desimciu
+                int reiksme = (ats.charAt(dabartinePozicija + 1) - '0') + daugyba / 10; //1, 0, 7, 1, 0, 6, 1, 0
 
-                int saugoti = ats.charAt(dabartinePozicija) - '0';
-                int daugyba = skaitmuo1 * skaitmuo2 + saugoti;
-
-                //Nustatoma kiekvieno sudauginto skaitmens vieta daugybos rezultate
-
-                ats.setCharAt(dabartinePozicija, (char)(daugyba % 10 + '0'));
-
-                // Issaugome daugybos rezultata "tarp desimtainiu skaiciu"
-
-                int reiksme = (ats.charAt(dabartinePozicija + 1) - '0') + daugyba / 10;
-
-                // kiekviena gauta reiksme pridedame i kita pozicija
-                ats.setCharAt(dabartinePozicija + 1, (char)(reiksme + '0'));
-            }
+                // kiekviena gauta reiksme pridedame i sekancia pozicija
+                ats.setCharAt(dabartinePozicija + 1, (char) (reiksme + '0'));
+                                                                         //810000, 831000, 837000, 888000,
+            }                                                            //888100, 888600, 880800, 880610, 880650
         }
-        // Pasaliname 0 is atsakymo pabaigos
+        // Jei skaicius pirmas nuo galo lygus nuliui, pasaliname.
         if (ats.charAt(ats.length() - 1) == '0') {
             ats.deleteCharAt(ats.length() - 1);
         }
